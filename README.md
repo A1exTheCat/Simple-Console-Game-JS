1106552dedbd533e46dada0936c373205eedc446
HomePC <alexzep@yandex.ru>
2022-10-07 23:36:39 +0300

    lvl4 commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/A1exTheCat/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/A1exTheCat/frontend-project-lvl1/actions)
+Maintainability
+<a href="https://codeclimate.com/github/A1exTheCat/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/b836345c2a4843b4c995/maintainability" /></a>
--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,5 +1,7 @@
-import readlineSync from "readline-sync";
-export const userName = () => {
-  const userNameAnswer = readlineSync.question("May I have your name? ");
-  return console.log("Hi " + userNameAnswer + "!");
+import readlineSync from 'readline-sync';
+
+const userName = () => {
+  const userNameAnswer = readlineSync.question('May I have your name? ');
+  return console.log('Hi ' + userNameAnswer + '!');
 };
+export default userName;

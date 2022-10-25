4b8a5be2024456417f6b643b381ab2633cefa075
HomeNote <alexzep@yandex.ru>
2022-10-25 14:32:10 +0200

    Final v 0.1

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const userName = () => {
   console.log('Welcome to the Brain Games!');
   const userNameAnswer = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userNameAnswer}!`);
+  console.log(`Hello, ${userNameAnswer}!`);
   return userNameAnswer;
 };
 export default userName;
